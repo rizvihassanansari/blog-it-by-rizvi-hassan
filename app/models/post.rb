dcbcc5b68055a3e31c6ce65406f9c67a3d1a3b74
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-08 21:07:55 +0530

    added slug to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,48 @@ class Post < ApplicationRecord
   validates :title,
     presence: true,
     length: { maximum: MAX_TITLE_LENGTH }
-
   validates :description,
     presence: true,
     length: { maximum: MAX_DESCRIPTION_LENGTH }
-
+  validates :slug, uniqueness: true
   validates_inclusion_of :is_bloggable, in: [true, false]
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    # def set_slug
+    #   title_slug = title.parameterize
+    #   regex_pattern = "slug #{Constants::DB_REGEX_OPERATOR} ?"
+    #   latest_post_slug = Post.where(
+    #     regex_pattern,
+    #     "^#{title_slug}$|^#{title_slug}-[0-9]+$"
+    #   ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+    #   slug_count = 0
+    #   if latest_post_slug.present?
+    #     slug_count = latest_post_slug.split("-").last.to_i
+    #     only_one_slug_exists = slug_count == 0
+    #     slug_count = 1 if only_one_slug_exists
+    #   end
+    #   slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+    #   self.slug = slug_candidate
+    # end
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = title.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Post.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if will_save_change_to_slug? && self.persisted?
+        errors.add(:slug, i18n.t("task.slug.immutable"))
+      end
+    end
 end

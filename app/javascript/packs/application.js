3a20af35e37c4258b9af8fb8cbc3ef4ff140392b
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-10 11:30:53 +0530

    Added toaster notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,12 @@ import ReactRailsUJS from "react_ujs";
 import i18n from "common/i18n";
 import App from "../src/App";
 
-import { setAuthHeaders } from "apis/axios";
+import { setAuthHeaders, registerIntercepts } from "apis/axios";
 import { initializeLogger } from "common/logger";
 
 initializeLogger();
 setAuthHeaders();
+registerIntercepts();
 
 const componentsContext = { App };
 ReactRailsUJS.getConstructor = name => {
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import { ShowPosts, CreatePost } from "./components/Posts";
 import routes from "./routes";
 
 const App = () => (
   <Router>
+    <ToastContainer />
     <Switch>
       <Route exact component={ShowPosts} path={routes.root} />
       <Route exact component={CreatePost} path={routes.posts.create} />

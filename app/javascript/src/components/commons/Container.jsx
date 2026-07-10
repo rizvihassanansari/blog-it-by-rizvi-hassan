e22e3254ef5cc6f798de9579c464a03887006cfc
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-10 10:33:44 +0530

    Made page responsive

--- app/javascript/src/components/commons/Container.jsx
+++ app/javascript/src/components/commons/Container.jsx
@@ -6,17 +6,16 @@ import PropTypes from "prop-types";
 import NavBar from "./NavBar";
 
 const Container = ({ children, className = "" }) => (
-  <>
+  <main className=" flex h-screen w-full overflow-hidden">
     <NavBar />
-    <div
-      className={classnames(
-        "mx-auto h-screen min-h-screen max-w-6xl px-6 pt-6",
-        [className]
-      )}
-    >
-      {children}
+    <div className="h-screen w-full overflow-scroll">
+      <div
+        className={classnames("mx-auto h-fit max-w-6xl px-6 pt-6", [className])}
+      >
+        {children}
+      </div>
     </div>
-  </>
+  </main>
 );
 
 Container.propTypes = {
--- app/javascript/src/components/commons/NavBar/index.jsx
+++ app/javascript/src/components/commons/NavBar/index.jsx
@@ -6,7 +6,7 @@ import { LINKS } from "./constants";
 import NavLink from "./Link";
 
 const NavBar = () => (
-  <div className="fixed left-0 top-0 flex h-full w-14 flex-col items-center gap-4 border-r py-6">
+  <div className="relative left-0 top-0 flex h-full w-14 flex-col items-center gap-4 border-r py-6">
     {LINKS.map(params => (
       <NavLink key={params.to} {...params} />
     ))}

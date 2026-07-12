18a94c400acd02051e6ee5569e159f2475389f78
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-12 20:30:36 +0530

    Made json as default response from backend

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  resources :posts, only: %i[index create show], params: %i[slug page categories]
-  resources :users, only: :index
-  resources :categories, only: %i[index create], param: :search
+  constraints(lambda { |req| req.format == :json }) do
+    resources :posts, only: %i[index create show], params: %i[slug page categories]
+    resources :users, only: :index
+    resources :categories, only: %i[index create], param: :search
+  end
 
   root "home#index"
   get "*path", to: "home#index", via: :all

748730197dc666a55c44f450cbe4d714a175e384
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-15 14:06:53 +0530

    Added tests for user model

--- Gemfile
+++ Gemfile
@@ -77,6 +77,8 @@ group :development, :test do
 end
 
 gem "bcrypt", "~> 3.1.13"
+gem "byebug"
 gem "connection_pool", "~> 2.4"
 gem "data_migrate"
 gem "react-rails", "~> 2.7.1"
+gem "simplecov", require: false, group: :test
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,77 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "test_password",
+      password_confirmation: "test_password")
+
+    @organization = Organization.new(name: "Test Organization")
+    @user.organization = @organization
+  end
+
+  def test_user_should_not_be_valid_and_saved_without_name
+    @user.name = ""
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_name_should_be_of_valid_length
+    @user.name = "a" * (User::MAX_NAME_LENGTH + 1)
+    assert @user.invalid?
+  end
+
+  def test_user_should_not_be_valid_and_saved_without_email
+    @user.email = ""
+    assert_not @user.valid?
+    assert_not @user.save
+    assert_includes @user.errors.full_messages, "Email can't be blank"
+  end
+
+  def test_user_should_not_be_valid_and_saved_if_email_not_unique
+    @user.save!
+
+    test_user = @user.dup
+    assert_not test_user.valid?
+
+    assert_includes test_user.errors.full_messages, "Email has already been taken"
+  end
+
+  def test_email_should_be_saved_in_lowercase
+    uppercase_email = "SAM@EXAMPLE.COM"
+    @user.email = uppercase_email
+    @user.save!
+
+    assert_equal uppercase_email.downcase, @user.email
+  end
+
+  def test_user_should_not_be_saved_without_password
+    @user.password = nil
+    assert_not @user.valid?
+    assert_not @user.save
+    assert_includes @user.errors.full_messages, "Password can't be blank"
+  end
+
+  def user_should_not_be_saved_without_password_confirmation
+    @user.password_confirmation = nil
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "Password confirmation can't be blank"
+  end
+
+  def test_user_should_have_matching_password_and_password_confirmation
+    @user.password_confirmation = "#{@user.password}-random"
+    assert_not @user.valid?
+    assert_includes @user.errors.full_messages, "Password confirmation doesn't match Password"
+  end
+
+  def test_user_should_have_unique_auth_token
+    @user.save!
+    second_user = User.create!(
+      name: "Oliver Smith", email: "oliver@example.com", password: "welcome",
+      password_confirmation: "welcome", organization: @user.organization)
+
+    assert_not_same @user.authentication_token, second_user.authentication_token
+  end
 end

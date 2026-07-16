c8c6d5c82f3fa8909bf6e45c91619ce6de517cdc
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-16 10:45:57 +0530

    Fixed small bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "../stylesheets/application.scss";
 import ReactRailsUJS from "react_ujs";
 import i18n from "common/i18n";
-import App from "../src/App";
-
 import { setAuthHeaders, registerIntercepts } from "apis/axios";
+
+import App from "../src/App";
 import { initializeLogger } from "common/logger";
 
 initializeLogger();

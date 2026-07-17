ab69c54ebdf30e5ddd910b0329a782138792cd65
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-17 17:40:33 +0530

    Removed wrong tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,35 +3,4 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "index returns posts with categories in json" do
-    organization = Organization.create!(name: "Test Organization")
-    user = User.create!(
-      name: "Test User",
-      email: "test@example.com",
-      password: "password123",
-      password_confirmation: "password123",
-      organization:
-    )
-    category = Category.create!(name: "Ruby")
-    post = Post.create!(
-      title: "Hello world",
-      description: "A sample post",
-      is_bloggable: true,
-      user:,
-      organization:,
-      category_ids: [category.id]
-    )
-
-    get posts_url(format: :json), headers: {
-      "X-Auth-Email" => user.email,
-      "X-Auth-Token" => user.authentication_token
-    }
-
-    assert_response :success
-
-    response_json = JSON.parse(response.body)
-    assert_equal 1, response_json["total_results"]
-    assert_equal post.id, response_json["posts"].first["id"]
-    assert_equal "Ruby", response_json["posts"].first["categories"].first["name"]
-  end
 end

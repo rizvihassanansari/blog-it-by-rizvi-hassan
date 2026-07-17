524e3aabe61ac38d8c494735656945f3278fabdf
Md Rizvi Hassan Ansari <rizvi.ansari@pixelcompute.com>
2026-07-17 13:06:51 +0530

    Added upvotes and downvotes ui

--- app/javascript/src/components/Posts/List/Item.jsx
+++ app/javascript/src/components/Posts/List/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { DownArrowCircle, UpArrowCircle } from "@bigbinary/neeto-icons";
 import { Typography } from "@bigbinary/neetoui";
 import { Link } from "react-router-dom";
 import routes from "routes";
@@ -14,17 +15,30 @@ const Item = ({
   user: { name = "" } = {},
   categories,
 }) => (
-  <li className="border-b border-gray-300 pb-2 pt-6">
-    <Link to={`${routes.posts.show.replace(":slug", slug)}`}>
-      <Typography className="mb-2 hover:text-blue-700" style="h2" weight="bold">
-        {title}
+  <li className="flex h-full items-center justify-between border-b border-gray-300 pb-4 pt-6">
+    <div className="w-full">
+      <Link to={`${routes.posts.show.replace(":slug", slug)}`}>
+        <Typography
+          className="mb-2 hover:text-blue-700"
+          style="h2"
+          weight="bold"
+        >
+          {title}
+        </Typography>
+      </Link>
+      <CategoryTags {...{ categories }} />
+      <Typography style="body3" weight="bold">
+        {name}
       </Typography>
-    </Link>
-    <CategoryTags {...{ categories }} />
-    <Typography style="body3" weight="bold">
-      {name}
-    </Typography>
-    <Typography style="nano">{formatDate(updatedAt)}</Typography>
+      <Typography style="nano">{formatDate(updatedAt)}</Typography>
+    </div>
+    <div className="flex h-full w-10 flex-col items-center gap-1 ">
+      <UpArrowCircle className="cursor-pointer rounded-full text-green-500 transition-colors duration-300 hover:bg-green-500 hover:text-white" />
+      <Typography style="body2" weight="bold">
+        50
+      </Typography>
+      <DownArrowCircle className="cursor-pointer rounded-full text-gray-500 transition-colors duration-300 hover:bg-red-500 hover:text-white" />
+    </div>
   </li>
 );
 
